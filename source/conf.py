ef5f694e38cd1270f1cee5b1785b6a363e86fa6b
rickey22 <luoruiqi0202@zhejianglab.com>
2022-12-22 15:39:34 +0800

    sphinx-sw-version1

--- source/conf.py
+++ source/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys, os
 
 project = 'Intelligent Software Design'
 copyright = '2022, Rickey'
@@ -13,11 +14,14 @@ release = 'v1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
+extensions = ['myst_parser']
 templates_path = ['_templates']
 exclude_patterns = []
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 language = 'zh_CN'
 
@@ -26,8 +30,46 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
 extensions = [
      'recommonmark',
-     'sphinx_markdown_tables'
- ]
+     'sphinx_markdown_tables',
+    #  'myst_parser',
+     'sphinx.ext.autodoc',
+     'sphinx.ext.doctest',
+     'sphinx.ext.intersphinx',
+     'sphinx.ext.todo',
+     'sphinx.ext.coverage',
+     'sphinx.ext.mathjax',
+    #  'sphinx.ext.pngmath',
+     'sphinx.ext.napoleon',
+     'sphinx-mathjax-offline' ]
+
+# inside conf.py
+latex_engine = 'xelatex'
+latex_elements = {
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+latex_show_urls = 'footnote'
+
+extensions.append('sphinx.ext.todo')
+extensions.append('sphinx.ext.autodoc')
+#extensions.append('sphinx.ext.autosummary')
+extensions.append('sphinx.ext.intersphinx')
+extensions.append('sphinx.ext.mathjax')
+extensions.append('sphinx.ext.viewcode')
+extensions.append('sphinx.ext.graphviz')
+
+extensions = ['myst_parser', 'sphinx.ext.mathjax']
